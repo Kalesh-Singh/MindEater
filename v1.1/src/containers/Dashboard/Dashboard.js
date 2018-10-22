2fbdec14ecb083211a2e383fee11d7a5ca9f23e8
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-22 02:24:20 -0700

    Added routing where the navigation items are displayed on any page other than the sign in / up page

---
+++ v1.1/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div style={{marginTop: '100px'}}>
+                <h1>Dashboard</h1>
+                <p>Some awesome dashboard content!</p>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
