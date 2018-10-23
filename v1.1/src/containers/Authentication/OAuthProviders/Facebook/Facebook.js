7e8ea4d4f8a44b865bb74149bbee63e8aa69fc09
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-22 22:39:52 -0700

    Fixed firebase imports

--- v1.1/src/containers/Authentication/OAuthProviders/Facebook/Facebook.js
+++ v1.1/src/containers/Authentication/OAuthProviders/Facebook/Facebook.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Button} from "@material-ui/core";
 import FacebookIcon from "../../../../assets/svg/FacebookIcon/FacebookIcon";
-import firebase from "firebase";
+import firebase from "firebase/app";
 import fire from "../../../../fire";
 
 class Facebook extends Component {
