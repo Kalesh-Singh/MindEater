add8f72b50338588bea754d5b504c1879091be6f
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-25 11:15:24 -0700

    Questions component

--- v1.1/src/components/CreateQuestion/Options/Options.js
+++ v1.1/src/components/CreateQuestion/Options/Options.js
@@ -2,9 +2,31 @@ import React, {Component} from 'react';
 import FormLabel from "@material-ui/core/FormLabel/FormLabel";
 import RadioGroup from "@material-ui/core/RadioGroup/RadioGroup";
 import FormControl from "@material-ui/core/FormControl/FormControl";
+import TextField from "@material-ui/core/TextField/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
+import Radio from "@material-ui/core/Radio/Radio";
 
 class Options extends Component {
     render() {
+
+        const options = this.props.options.map((option, index) => (
+            <FormControlLabel
+                key={index}
+                value={this.props.options[index]}
+                control={<Radio/>}
+                disabled={this.props.options[index] === ''}
+                label={
+                    <TextField
+                        name={'Option ' + (index + 1)}
+                        label={'Option ' + (index + 1)}
+                        type='text'
+                        margin='normal'
+                        value={this.props.options[index]}
+                        onChange={this.props.textChanged(index)}
+                    />
+                }/>
+        ));
+
         return (
             <FormControl component="fieldset">
                 <FormLabel component="legend">Options</FormLabel>
