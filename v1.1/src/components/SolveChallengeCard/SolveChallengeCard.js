dea300a71c62bb5a0fa6c084ad69b4456d9d36e2
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-01 13:48:25 -0700

    Get challenges in dashboard

--- v1.1/src/components/SolveChallengeCard/SolveChallengeCard.js
+++ v1.1/src/components/SolveChallengeCard/SolveChallengeCard.js
@@ -2,12 +2,9 @@ import React, {Component} from 'react';
 import classes from "./SolveChallengeCard.module.css";
 import Card from "@material-ui/core/Card/Card";
 import CardContent from "@material-ui/core/CardContent/CardContent";
-import DeleteIcon from "@material-ui/core/SvgIcon/SvgIcon";
-import ChallengeDialog from "../ChallengeDialog/ChallengeDialog";
 import ListItem from "@material-ui/core/ListItem/ListItem";
 import CardActionArea from "@material-ui/core/CardActionArea/CardActionArea";
 import Typography from "@material-ui/core/Typography/Typography";
-import CardActions from "@material-ui/core/CardActions/CardActions";
 
 class SolveChallengeCard extends Component {
     render() {
@@ -29,15 +26,6 @@ class SolveChallengeCard extends Component {
                             </Typography>
                         </CardContent>
                     </CardActionArea>
-                    {/*<CardActions style={{alignSelf: 'flex-end'}}>
-                        <Button
-                            color="secondary"
-                            onClick={this.deleteChallenge}
-                        >
-                            Delete
-                            <DeleteIcon style={{marginLeft: '8px'}}/>
-                        </Button>
-                    </CardActions>*/}
                 </Card>
                 {/*<ChallengeDialog
                     open={this.state.open}
--- v1.1/src/containers/Dashboard/Dashboard.js
+++ v1.1/src/containers/Dashboard/Dashboard.js
@@ -1,12 +1,88 @@
 import React, {Component} from 'react';
-import Grid from "@material-ui/core/Grid/Grid";
-import classes from "../Authentication/Authentication.module.css";
-import SolveChallengeStepper from "../../components/SolveChallengeStepper/SolveChallengeStepper";
+import fire from "../../fire";
+import SolveChallengeCard from "../../components/SolveChallengeCard/SolveChallengeCard";
+import classes from "./Dashboard.module.css";
+import List from "@material-ui/core/List/List";
 
 class Dashboard extends Component {
 
+    state = {
+        challenges: []
+    };
+
+    setListeners = () => {
+        fire.database().ref('/challenges/')
+            .on('child_added', snapshot => {
+                console.log('Child Added - challenges');
+                const challenge = snapshot.val();
+                challenge.id = snapshot.key;
+                const updatedChallenges = [...this.state.challenges];
+                updatedChallenges.push(challenge);
+                this.setState({challenges: updatedChallenges});
+            });
+        fire.database().ref('/challenges/')
+            .on('child_removed', snapshot => {
+                console.log('Child Removed - challenges');
+                const challengeId = snapshot.key;
+                const updatedChallenges
+                    = this.state.challenges.filter((challenge) => (challenge.id !== challengeId));
+                this.setState({challenges: updatedChallenges});
+            });
+        fire.database().ref('/challenges/')
+            .on('child_changed', snapshot => {
+                console.log('Child Changed - challenges');
+                const challengeId = snapshot.key;
+                const updatedChallenges = [...this.state.challenges];
+                const oldChallengeIndex = updatedChallenges
+                    .findIndex(challenge => (challenge.id === challengeId));
+                const updatedChallenge = snapshot.val();
+                updatedChallenge.id = challengeId;
+                updatedChallenges[oldChallengeIndex] = updatedChallenge;
+                this.setState({challenges: updatedChallenges});
+            });
+    };
+
+    componentDidMount() {
+        console.log('Dashboard did mount');
+        this.setListeners();
+
+        fire.database().ref('/challenges/')
+            .once('value')
+            .then(snapshot => {
+                const challengesObject = snapshot.val();
+                const updatedChallenges = [];
+                for (let challengeId in challengesObject) {
+                    const challenge = challengesObject[challengeId];
+                    challenge.id = challengeId;
+                    updatedChallenges.push(challenge);
+                }
+                this.setState({challenges: updatedChallenges});
+            });
+    }
+
     render() {
-        {/*<Grid
+        const challenges = this.state.challenges.map(challenge => (
+            <SolveChallengeCard
+                key={challenge.id}
+                challenge={challenge}
+            />
+        ));
+
+        return (
+            <div style={{marginTop: '100px'}}>
+                <h1>Dashboard</h1>
+                <p>Some awesome dashboard content!</p>
+                <List className={classes.root}>
+                    {challenges}
+                </List>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
+
+/*<Grid
                 container
                 spacing={0}
                 direction="column"
@@ -22,15 +98,3 @@ class Dashboard extends Component {
                 </Grid>
                 <SolveChallengeStepper/>
             </Grid>*/
-        }
-
-        return (
-            <div style={{marginTop: '100px'}}>
-                <h1>Dashboard</h1>
-                <p>Some awesome dashboard content!</p>
-            </div>
-        );
-    }
-}
-
-export default Dashboard;
