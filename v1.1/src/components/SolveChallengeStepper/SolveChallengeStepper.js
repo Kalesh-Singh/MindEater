82ac6c66724334a2a06cbdd75a2d882fd7f0b95a
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-01 11:55:40 -0700

    Solve Challenge Stepper

---
+++ v1.1/src/components/SolveChallengeStepper/SolveChallengeStepper.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import Stepper from "@material-ui/core/Stepper/Stepper";
+import Step from "@material-ui/core/Step/Step";
+import StepLabel from "@material-ui/core/StepLabel/StepLabel";
+import StepContent from "@material-ui/core/StepContent/StepContent";
+import Typography from "@material-ui/core/Typography/Typography";
+import Button from "@material-ui/core/Button/Button";
+
+
+class SolveChallengeStepper extends Component {
+
+    state = {
+        activeStep: 0,
+    };
+
+    getSteps = () => {
+        return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+    };
+
+    getStepContent = (step) => {
+        switch (step) {
+            case 0:
+                return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+            case 1:
+                return 'An ad group contains one or more ads which target a shared set of keywords.';
+            case 2:
+                return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+            default:
+                return 'Unknown step';
+        }
+    };
+
+    handleNext = () => {
+        this.setState(state => ({activeStep: state.activeStep + 1}));
+    };
+
+    render() {
+
+        const steps = this.getSteps();
+
+        return (
+                <Stepper activeStep={this.state.activeStep} orientation="vertical">
+                    {steps.map((label, index) => {
+                        return (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                                <StepContent>
+                                    <Typography>{this.getStepContent(index)}</Typography>
+                                    <div>
+                                        <div>
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                onClick={this.handleNext}
+                                            >
+                                                {this.state.activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </StepContent>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
+        );
+    }
+}
+
+export default SolveChallengeStepper;
--- v1.1/src/containers/Dashboard/Dashboard.js
+++ v1.1/src/containers/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Grid from "@material-ui/core/Grid/Grid";
 import classes from "../Authentication/Authentication.module.css";
+import SolveChallengeStepper from "../../components/SolveChallengeStepper/SolveChallengeStepper";
 
 class Dashboard extends Component {
 
@@ -15,12 +16,13 @@ class Dashboard extends Component {
                 justify="top"
                 style={{minHeight: '100vh'}}
             >
-                <Grid item xs={12} style={{background:"red"}}>
-            <div style={{marginTop: '100px'}}>
-                <h1>Dashboard</h1>
-                <p>Some awesome dashboard content!</p>
-            </div>
+                <Grid item xs={12} style={{background: "red"}}>
+                    <div style={{marginTop: '100px'}}>
+                        <h1>Dashboard</h1>
+                        <p>Some awesome dashboard content!</p>
+                    </div>
                 </Grid>
+                <SolveChallengeStepper/>
             </Grid>
         );
     }
