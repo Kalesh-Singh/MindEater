d260c442549dc6fbe74b83f739ea7083cc0b3edf
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-01 17:29:08 -0700

    SolveChallengeSummary component

---
+++ v1.1/src/components/SolveChallengeSummary/SolveChallengeSummary.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class SolveChallengeSummary extends Component {
+
+    // Expected props
+    // 1. open          (bool)
+    // 2. closed        (func)
+    // 3. closeStepper  (func)
+    // 4. steps         (Array)     Can get from the getStep() function.
+    // 5. score         (number)
+
+    // TODO: Decide which component will push to firebase.
+
+    render() {
+        // Make this some sort of dialog pop up
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default SolveChallengeSummary;
