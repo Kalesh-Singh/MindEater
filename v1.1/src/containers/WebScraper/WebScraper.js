5dc71289613ad9d4747075386e6fa20bb5dec892
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-06 01:06:13 -0800

    Working client side web scraper

--- v1.1/src/containers/WebScraper/WebScraper.js
+++ v1.1/src/containers/WebScraper/WebScraper.js
@@ -3,8 +3,10 @@ import TextField from "@material-ui/core/TextField/TextField";
 import Button from "@material-ui/core/Button/Button";
 import $ from "jquery";
 import cheerio from "cheerio";
+import fire from "../../fire.js"
 
 import classes from "./WebScraper.module.css";
+
 
 class WebScraper extends Component {
     state = {
@@ -16,7 +18,45 @@ class WebScraper extends Component {
         this.setState({title: event.target.value});
     };
 
-    getImg = () => {
+    getBlob = (url) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = event => {
+                const blob = xhr.response;
+                resolve(blob);
+            };
+            xhr.onerror = reject;
+            xhr.open('GET', url);
+            xhr.send();
+        });
+    };
+
+    imgURLForChallenge = (oldURL, challengeId) => {
+        const picRef = fire.storage().ref(/challenges/ + challengeId);
+
+        this.getBlob(oldURL)
+            .then(blob => {
+                return picRef.put(blob)
+            })
+            .then(() => {
+                return picRef.getDownloadURL();
+            })
+            .then(url => {
+                const updates = {};
+                updates['/challenges/' + challengeId + '/imgURL'] = url;
+                return fire.database().ref().update(updates);
+
+            })
+            .then(() => {
+                console.log('Successfully updated challenge image url');
+            })
+            .catch(error => {
+                alert(error.message);
+            })
+    };
+
+    getImage = () => {
         const searchTerm = this.state.title.split(' ').join('+');
         const imgUrl = 'https://www.shutterstock.com/search?search_source=base_landing_page&language=en&searchterm='
             + searchTerm + '&image_type=all';
@@ -30,8 +70,8 @@ class WebScraper extends Component {
                 const $ = cheerio.load(html);
                 const images = $(".img-wrap");
                 if (images[0]) {
-                    const challengeImg = images[0].children[1].attribs.src;
-                    this.setState({imgUrl: challengeImg});
+                    const challengeImgUrl = images[0].children[1].attribs.src;
+                    this.imgURLForChallenge(challengeImgUrl, '123456');
                 } else {
                     // TODO: Set the state to the default challenge image.
                 }
