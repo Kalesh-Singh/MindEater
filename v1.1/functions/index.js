f7219df40ed6d64fdf55df4e7441f8ecc6f0a9c0
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-05 23:47:21 -0800

    Retrieving the page with https

--- v1.1/functions/index.js
+++ v1.1/functions/index.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const https = require('https');
 const cheerio = require('cheerio');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -42,48 +42,62 @@ exports.getChallengeImage = functions.database.ref('/challenges/{pushId}/title')
                 promise = Promise.reject("Something went wrong")
             });*/
 
-       /* $.ajax({
-            url,
-            dataType: 'json',
-            success: (response) => {
-                console.log('response', response);
-                const html = response.contents;
-                const $ = cheerio.load(html);
-                const images = $(".img-wrap");
-                if (images[0]) {
-                    const challengeImg = images[0].children[1].attribs.src;
-                    console.log("Image URL", challengeImg);
-                } else {
-                    // TODO: Set the state to the default challenge image.
-                }
-                promise = Promise.resolve();
+        /* $.ajax({
+             url,
+             dataType: 'json',
+             success: (response) => {
+                 console.log('response', response);
+                 const html = response.contents;
+                 const $ = cheerio.load(html);
+                 const images = $(".img-wrap");
+                 if (images[0]) {
+                     const challengeImg = images[0].children[1].attribs.src;
+                     console.log("Image URL", challengeImg);
+                 } else {
+                     // TODO: Set the state to the default challenge image.
+                 }
+                 promise = Promise.resolve();
+             }
+         });*/
+
+        /*axios.get(url, {
+            proxy: false,
+        })
+            .then(response => {
+                console.log(response);
+                success = true;
+            })
+            .catch(error => {
+                console.log(error.message);
+                success = false;
+            });
+            */
+
+        /* request(url, { json: true }, (err, res, body) => {
+            if (err) {
+                console.log(err);
+                success = false;
+                return err;
             }
-        });*/
+            console.log('res ==', res);
+            success = true;
+            console.log('body == ', body);
+         });*/
 
-       /*axios.get(url, {
-           proxy: false,
-       })
-           .then(response => {
-               console.log(response);
-               success = true;
-           })
-           .catch(error => {
-               console.log(error.message);
-               success = false;
-           });
-           */
-
-       request(url, { json: true }, (err, res, body) => {
-          if (err) {
-              console.log(err);
-              success = false;
-              return err;
-          }
-          console.log('res ==', res);
-          success = true;
-          console.log('body == ', body);
-       });
-
-
-        return (success) ? Promise.resolve() : Promise.reject();
+        return new Promise((resolve, reject) => {
+            let data = '';
+            const request = https.get(url, res => {
+                res.on('data', d => {
+                    data += d;
+                });
+                res.on('end', () => {
+                    console.log(data);
+                    resolve();
+                });
+            });
+            request.on('error', () => {
+                console.log('An error occurred whilst fetching the data');
+                reject();
+            });
+        });
     });
