7bdba7fbcfdd602185542ae34f883ced1b1ff892
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-06 21:24:57 -0800

    Saving modal component

--- v1.1/src/components/SavingModal/SavingModal.js
+++ v1.1/src/components/SavingModal/SavingModal.js
@@ -1,36 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
 import Typography from "@material-ui/core/Typography/Typography";
 import Modal from "@material-ui/core/Modal/Modal";
 import classes from "./SavingModal.module.css";
 
-class SavingModal extends Component {
+function SavingModal(props) {
+
     // Expected props
     // 1. open (which will be the saving state from the ChallengeDialog)
 
-    render() {
-        return (
-            <Modal
-                aria-labelledby="saving-modal-title"
-                aria-describedby="saving-modal-description"
-                open={this.props.open}
-            >
-                <div className={classes.Modal}>
-                    <CircularProgress
-                        variant="indeterminate"
-                        size={80}
-                        style={{marginBottom: "16px"}}
-                    />
-                    <Typography
-                        variant="h6"
-                        color="inherit"
-                    >
-                        Saving Challenge
-                    </Typography>
-                </div>
-            </Modal>
-        );
-    }
+    return (
+        <Modal
+            aria-labelledby="saving-modal-title"
+            aria-describedby="saving-modal-description"
+            open={props.open}
+        >
+            <div className={classes.Modal}>
+                <CircularProgress
+                    variant="indeterminate"
+                    size={80}
+                    style={{marginBottom: "16px"}}
+                />
+                <Typography
+                    variant="h6"
+                    color="inherit"
+                >
+                    Saving Challenge
+                </Typography>
+            </div>
+        </Modal>
+    );
 }
 
 export default SavingModal;
