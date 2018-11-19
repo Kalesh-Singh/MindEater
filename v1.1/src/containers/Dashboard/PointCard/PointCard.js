ba9f5d40ff5f70cdc37b51bb8df2116c3118521e
Jesus Nunez <nunezjesus@nunezjesus-macbookpro.roam.corp.google.com>
2018-11-19 00:45:35 -0800

    Further Styled

--- v1.1/src/containers/Dashboard/PointCard/PointCard.js
+++ v1.1/src/containers/Dashboard/PointCard/PointCard.js
@@ -7,27 +7,27 @@ import Card from "@material-ui/core/Card/Card";
 import CardActions from "@material-ui/core/CardActions/CardActions";
 import Divider from "@material-ui/core/Divider/Divider";
 import classes from "./PointCard.module.css"
+import Android from "@material-ui/icons/AndroidOutlined";
 
 class PointCard extends Component {
     render() {
         return (
             <div>
-                <Card>
+                <Card style={{width:"300px"}}>
                     <CardHeader style={{background: "#2096F3"}}/>
                         <CardMedia
                             title={"Points"}/>
+                    <div style ={{marginTop:20}}>
+                        <div className={classes.Box}>
+                            <Android style={{width:70, height:70, color:"white"}}/>
+                        </div>
+                    </div>
                         <CardContent>
                             <Typography gutterBottom variant={"h4"} component={"h1"}>
-                                Points
-                            </Typography>
-                            <Typography component={'p'}>
-                                User tier will be displayed here:
+                                Points:
                             </Typography>
                         </CardContent>
                     <Divider className={classes.Divider}/>
-                    <CardActions>
-                        <h4>This is where the points will be shown</h4>
-                    </CardActions>
                 </Card>
             </div>
         );
