ea822b357d0d7718e6eac5afa1a21b791cf8f679
Jesus Nunez <nunezjesus@nunezjesus-macbookpro.roam.corp.google.com>
2018-11-18 17:52:30 -0800

    Further Styled

--- v1.1/src/containers/Dashboard/Dashboard.js
+++ v1.1/src/containers/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from "./Dashboard.module.css";
 import ChallengesCard from "./ChallengesCard/ChallengesCard";
 import PointCard from "./PointCard/PointCard";
+import CompletedChallenges from "./CompletedChallenges/CompletedChallenges";
 
 
 class Dashboard extends Component {
@@ -10,8 +11,9 @@ class Dashboard extends Component {
 
         return (
             <div className={classes.Row}>
-                    <ChallengesCard/>
-                    <PointCard/>
+                    <ChallengesCard className={classes.root}/>
+                    <PointCard className={classes.root}/>
+                <CompletedChallenges className={classes.root}/>
             </div>
         );
     }
